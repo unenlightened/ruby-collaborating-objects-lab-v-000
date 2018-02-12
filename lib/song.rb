48bd190ee9fabee9fe9d97a6a3cdc46c21f8601f
Chely Ho <adept_inept@yahoo.com>
2018-02-12 17:33:00 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,7 @@ class Song
   end
 
   def self.new_by_filename(filename)   # :file_name {'Michael Jackson - Black or White - pop.mp3'}
-    name = filename.split(" - ")
+    name = filename.chomp(".mp3").split(" - ")
 
     new(name[1])
   end
